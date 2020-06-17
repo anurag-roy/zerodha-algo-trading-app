d7b35eae7bdf3e1d62de7bd8702bdcd700df28ba
Anurag Roy <anurag@2pirad.com>
2020-06-17 11:35:49 +0530

    Separated login to another module

--- onlyLogin.js
+++ onlyLogin.js
@@ -1,20 +1,21 @@
 const KiteConnect = require("kiteconnect").KiteConnect;
 const express = require("express");
-// const fs = require("fs");
-require("dotenv").config();
+const fs = require("fs");
 
 const app = express();
 app.use(express.json());
 
-const apiKey = process.env.API_KEY;
-const apiSecret = process.env.API_SECRET;
+const apiKey = "2rtrb58mdwkhu7s7";
+const apiSecret = "b5gxafr7w8tsu19h6vqa53jj5usk13fq";
 let accessToken;
 
 const kc = new KiteConnect({
   api_key: apiKey,
 });
 
-console.log(`Please click on this URL to get logged in: ${kc.getLoginURL()}`);
+const server = app.listen(8000, () => {
+  console.log(`Please click on this URL to get logged in: ${kc.getLoginURL()}`);
+});
 
 app.use("/login", (req, res) => {
   const requestToken = req.query.request_token;
@@ -29,9 +30,25 @@ app.use("/login", (req, res) => {
       console.log("Access Token set. ", accessToken);
     })
     .then(() => {
+      const contents = `API_KEY="${apiKey}"
+      API_SECRET="${apiSecret}"
+      ACCESS_TOKEN="${accessToken}"`;
+      writeEnvFile(contents);
+    })
+    .then(() => {
       res.send("Login flow successful!");
+    })
+    .then(() => {
+      server.close();
     })
     .catch((error) => {
       console.log("Error during login flow: ", error);
     });
 });
+
+const writeEnvFile = (contents) => {
+  fs.writeFileSync(".env", contents, (err) => {
+    if (err) console.log(err);
+    console.log(".env file updated.");
+  });
+};
